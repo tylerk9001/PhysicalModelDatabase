d00b421ee80443661889776b4b7782d279840b21
Jacob Stambaugh <jstambaugh@ycp.edu>
2019-03-21 16:34:51 -0400

    Added stats servlets and updated web xml

---
+++ CS320_GroupProject/src/edu/ycp/cs320/lab02/servlet/StatRServlet.java
@@ -0,0 +1,36 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import edu.ycp.cs320.lab02.controller.GuessingGameController;
+import edu.ycp.cs320.lab02.model.GuessingGame;
+
+public class StatRServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		System.out.println("Stat_R Servlet: doGet");	
+		
+		// call JSP to generate empty form
+		req.getRequestDispatcher("/_view/categories/construction/stat-r.jsp").forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		System.out.println("Stat_R Servlet: doPost");
+		
+		// now call the JSP to render the new page
+		req.getRequestDispatcher("/_view/categories/construction/stat-r.jsp").forward(req, resp);
+	}
+}
