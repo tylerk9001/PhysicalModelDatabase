095e5126f953e0a4dd8209a51fe9127cb54db81d
Tyler Kautz <bkautz@ycp.edu>
2019-03-21 15:34:28 -0400

    added servlets for const. and dynamics

---
+++ CS320_GroupProject/src/edu/ycp/cs320/lab02/servlet/ConstAServlet.java
@@ -0,0 +1,36 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import edu.ycp.cs320.lab02.controller.GuessingGameController;
+import edu.ycp.cs320.lab02.model.GuessingGame;
+
+public class ConstAServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		System.out.println("Const_A Servlet: doGet");	
+		
+		// call JSP to generate empty form
+		req.getRequestDispatcher("/_view/categories/construction/const-a.jsp").forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		System.out.println("Const_A Servlet: doPost");
+		
+		// now call the JSP to render the new page
+		req.getRequestDispatcher("/_view/categories/construction/const-a.jsp").forward(req, resp);
+	}
+}
