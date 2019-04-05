12714dd4903287e93f7dfc764e76cfa90a44cc5a
tswann2ycp <48060992+tswann2ycp@users.noreply.github.com>
2019-04-05 11:36:08 -0400

    Add files via upload

---
+++ CS320_GroupProject/src/edu/ycp/cs320/lab02/model/ProjectsAuthors.java
@@ -0,0 +1,29 @@
+package edu.ycp.cs320.lab02.model;
+
+public class ProjectsAuthors {
+	private int authorID;
+	private int projectID;
+	
+
+	public ProjectsAuthors() {
+	}
+	
+	public void setAuthorID(int authorID) {
+		this.authorID = authorID;
+	}
+	
+	public int getAuthorID() {
+		return authorID;
+	}
+	
+	public void setProjectID(int projectID) {
+		this.projectID = projectID;
+	}
+	
+	public int getProjectID() {
+		return projectID;
+	}
+	/////
+	/////
+	/////
+}
