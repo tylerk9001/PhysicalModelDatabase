1ff900fdbaed3e4d99fab54de0cb3e25a423b79c
Tyler Kautz <34490912+tylerk9001@users.noreply.github.com>
2019-04-09 16:56:48 -0400

    added signup controller

---
+++ CS320_GroupProject/src/edu/ycp/cs320/lab02/controller/SignUpController.java
@@ -0,0 +1,31 @@
+package edu.ycp.cs320.lab02.controller;
+
+import edu.ycp.cs320.booksdb.persist.DatabaseProvider;
+import edu.ycp.cs320.booksdb.persist.DerbyDatabase;
+import edu.ycp.cs320.booksdb.persist.IDatabase;
+import edu.ycp.cs320.lab02.model.UserAccount;
+
+public class SignUpController {
+	
+	private UserAccount model;
+	private IDatabase database;
+	boolean accountCreationSuccessful = false;
+	
+	public SignUpController() {
+		DatabaseProvider.setInstance(new DerbyDatabase());
+		database = DatabaseProvider.getInstance();
+	}
+	
+	public void setModel(UserAccount model) {
+		this.model = model;
+	}
+	
+	public UserAccount getModel() {
+		return model;
+	}
+	
+	public boolean createAccount(UserAccount model) {
+		accountCreationSuccessful = database.createAccount(model.getEmail(), model.getPassword(), model.getLastName(), model.getFirstName());
+		return accountCreationSuccessful;
+	}
+}	 
