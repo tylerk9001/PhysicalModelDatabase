b6b376bd7c88c2c5549afc2059ad891d423bb869
tswann2ycp <48060992+tswann2ycp@users.noreply.github.com>
2019-04-12 19:09:01 -0400

    Add files via upload
    search model

---
+++ CS320_GroupProject/src/edu/ycp/cs320/lab02/model/Search.java
@@ -0,0 +1,18 @@
+package edu.ycp.cs320.lab02.model;
+
+public class Search {
+	private String search;
+
+
+	public Search() {
+	}
+	
+	public String getSearch () {
+		return search;
+	}
+	
+	public void setSearch(String search) {
+		this.search = search;
+	}
+	
+}
